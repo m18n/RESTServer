c74b9c1be8e8dba6cf76d09cac94ab3718ea23c9
m5m14 <20xim04@gmail.com>
2023-11-14 02:29:13 +0200

    mutex

--- include/tasker_manager.h
+++ include/tasker_manager.h
@@ -10,6 +10,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include<mutex>
 namespace server{
 using t_json = nlohmann::json;
 
@@ -50,65 +51,111 @@ struct event
     bool process = false;
 };
 void init_event(event *ev);
+
 struct respon_id
 {
     int id = -1;
 };
+class mutex_n{
+public:
+mutex_n()=default;
+    void lock();
+    void unlock();
+private:
+    int n=0;
+    std::mutex mt;
+};
+
 class group_clients
 {
 public:
     group_clients()
     {
+        mt_client.lock();
+        mt_event.lock();
+        mt_respon_id.lock();
+        mt_var.lock();
         group = "";
         clients.resize(25);
         events.resize(100);
         ids.resize(100);
+        mt_client.unlock();
+        mt_event.unlock();
+        mt_respon_id.unlock();
+        mt_var.unlock();
+    }
+      group_clients(const group_clients& other)
+    {
+        // Implement the copy constructor logic here
     }
 
+    // Move constructor
+    group_clients(group_clients&& other) noexcept
+    {
+        // Implement the move constructor logic here
+    }
 private:
 std::string generate_hash_event(std::string strindex,std::string count_restart){
     std::string currentTime = std::to_string(time(nullptr));
     std::string mil=std::to_string(clock());
-    return sha256(server_hash + group + currentTime + strindex+mil+ count_restart);
+    mt_var.lock();
+    std::string hash=sha256(server_hash + group + currentTime + strindex+mil+ count_restart);
+    mt_var.unlock();
+    return hash;
 }
 public:
     void set_group(std::string group)
     {
+        mt_var.lock();
         this->group = group;
+        mt_var.unlock();
     }
     void init(std::string server_hash)
     {
+        mt_var.lock();
         this->server_hash = server_hash;
+        mt_var.unlock();
     }
     std::string get_group()
     {
-        return group;
+        
+        mt_var.lock();
+        std::string gr=group;
+        mt_var.unlock();
+        return gr;
     }
 
     respon_id get_respon_id()
     {
+        mt_respon_id.lock();
+        respon_id r;
         for (int i = 0; i < ids.size(); i++)
         {
             if (ids[i].id == -1)
             {
                 ids[i].id = i;
-                return ids[i];
+                r=ids[i];
+                mt_respon_id.unlock();
+                return r;
             }
         }
-        respon_id r;
         r.id = ids.size();
         ids.push_back(r);
+        mt_respon_id.unlock();
         return r;
     }
     void finish_respon_id(respon_id r)
     {
+        mt_respon_id.lock();
         if (r.id < ids.size())
         {
             ids[r.id].id = -1;
         }
+        mt_respon_id.unlock();
     }
     client get_new_client(std::string time, std::string ip)
     {
+        mt_client.lock();
         client cl;
         cl.hash_worker = sha256(time + ip);
         cl.group = this->group;
@@ -119,19 +166,23 @@ public:
             if (clients[i].busy == false)
             {
                 clients[i] = cl;
+                mt_client.unlock();
                 return cl;
             }
         }
         clients.push_back(cl);
+        mt_client.unlock();
         return cl;
     }
     void exit_client(std::string hash_worker)
     {
         
+        mt_client.lock();
         for (int i = 0; i < clients.size(); i++)
         {
             if (clients[i].hash_worker == hash_worker)
             {
+                mt_event.lock();
                 for(int j=0;j<events.size();j++){
                     if(events[j].process==true&&events[j].hash_worker==hash_worker){
                         events[j].hash_worker="";
@@ -140,29 +191,36 @@ public:
                         events[j].process=false;
                     }
                 }
+                mt_event.unlock();
                 init_client(&clients[i]);
                 break;
             }
         }
+        mt_client.unlock();
     }
     int start_event(std::string hash_worker,std::string event_id)
     {
-
+        mt_event.lock();
         for (int i = 0; i < events.size(); i++)
         {
             if (events[i].hash_event == event_id)
             {
-                if (events[i].process == true)
+                if (events[i].process == true){
+                    mt_event.unlock();
                     return -2;
+                }
                 events[i].process = true;
+                mt_event.unlock();
                 return 0;
             }
         }
+        mt_event.unlock();
         return -3;
+
     }
     int clear_event(std::string hash_worker,std::string event_id)
     {
-
+        mt_event.lock();
         for (int i = 0; i < events.size(); i++)
         {
             if (events[i].hash_event == event_id)
@@ -170,32 +228,41 @@ public:
                 if (events[i].process == true){
                     events[i].process=false;
                 }
+                mt_event.unlock();
                 return 0;
             }
         }
+        mt_event.unlock();
         return -2;
     }
     int end_event(std::string event_id)
     {
+        mt_event.lock();
         for (int i = 0; i < events.size(); i++)
         {
             if (events[i].hash_event == event_id)
             {
+                mt_var.lock();
                 if (events[i].json["meta"]["$server_hash"] == server_hash && events[i].json["meta"]["$type_event"] == "res")
                 {
                     respon_id r;
                     r.id = events[i].json["meta"]["$respon_id"];
                     finish_respon_id(r);
+                    
                 }
                 init_event(&events[i]);
+                mt_event.unlock();
+                mt_var.unlock();
                 return 0;
             }
         }
+        mt_client.unlock();
+        mt_event.unlock();
         return -2;
     }
     int add_new_event(event ev, std::string server_hash)
     {
-        
+        mt_event.lock();
         time_t currentTime = time(nullptr);
         std::string time = std::to_string(currentTime);
         std::string strindex;
@@ -204,22 +271,25 @@ public:
         {
             if (events[i].busy == false)
             {
+                ev.hash_event = generate_hash_event(strindex,std::to_string(ev.count_restart));
                 events[i] = ev;
                 strindex = std::to_string(i);
-                events[i].hash_event = generate_hash_event(strindex,std::to_string(ev.count_restart));
+                mt_event.unlock();
                 if (ev.json["meta"]["$type_event"] == "req")
-                    return events[i].json["meta"]["$respon_id"];
+                    return ev.json["meta"]["$respon_id"];
                 return -2;
             }
         }
         strindex = events.size();
         ev.hash_event= generate_hash_event(strindex,std::to_string(ev.count_restart));
         events.push_back(ev);
+        mt_event.unlock();
         if (ev.json["meta"]["$type_event"] == "req")
             return ev.json["meta"]["$respon_id"];
         return -2;
     }
     void ping_client(){
+        mt_client.lock();
         for (int i = 0; i < clients.size(); i++)
         {
             if (clients[i].busy==true)
@@ -231,9 +301,12 @@ public:
                 }
             }
         }
+        mt_client.unlock();
     }
     std::string get_events_json(std::string hash_worker)
     {
+        mt_client.lock();
+        mt_event.lock();
         static std::string respon;
         
         respon.resize(0);
@@ -272,6 +345,8 @@ public:
             }
         }
         char* ad=&respon[0];
+        mt_client.unlock();
+        mt_event.unlock();
         if(respon=="")
             return "{}";
         return respon;
@@ -283,6 +358,11 @@ private:
     std::vector<respon_id> ids;
     std::string group = "";
     std::string server_hash;
+    mutex_n mt_client;
+    mutex_n mt_respon_id;
+    mutex_n mt_event;
+    mutex_n mt_var;
+    
 };
 
 class tasker_manager
@@ -291,7 +371,6 @@ private:
     
     std::string gethash()
     {
-
         time_t currentTime = time(nullptr);
         // std::cout<<"TIME: "<<currentTime<<"\n";
         std::string hash = sha256(std::to_string(currentTime));
@@ -302,9 +381,10 @@ public:
     std::string name_server="tasker";
     tasker_manager()
     {
+        mt.lock();
         server_hash = gethash();
         last_check_client=time(nullptr);
-        
+        mt.unlock();
     }
 
     ~tasker_manager()
@@ -312,42 +392,58 @@ public:
     }
     int find_group(std::string group)
     {
+        mt.lock();
         for (int i = 0; i < clients_group.size(); i++)
         {
             if (clients_group[i].get_group() == group)
             {
+                mt.unlock();
                 return i;
             }
         }
+        mt.unlock();
         return -1;
     }
     std::string get_server_hash()
     {
-        return server_hash;
+        mt.lock();
+        std::string sr=server_hash;
+        mt.unlock();
+        return sr;
     }
     client get_new_client(std::string time, std::string ip, std::string group)
     {
+        mt.lock();
+        client cl;
         for (int i = 0; i < clients_group.size(); i++)
         {
             if (clients_group[i].get_group() == group)
             {
-                return clients_group[i].get_new_client(time, ip);
+                cl=clients_group[i].get_new_client(time, ip);
+                mt.unlock();
+                return cl;
             }
         }
         group_clients gr;
         gr.set_group(group);
         gr.init(server_hash);
         clients_group.push_back(std::move(gr));
-        return clients_group[clients_group.size() - 1].get_new_client(time, ip);
+        cl=clients_group[clients_group.size() - 1].get_new_client(time, ip);
+        mt.unlock();
+        return cl;
     }
     void exit_client(std::string hash_worker, std::string group)
     {
+        mt.lock();
         int index = find_group(group);
         if (index != -1)
             clients_group[index].exit_client(hash_worker);
+        mt.unlock();
     }
     std::string get_events_json(std::string group, std::string hash_worker)
     {
+        mt.lock();
+        std::string ev;
         //last_check_client=time(nullptr);
         time_t currentTime = time(nullptr);
         time_t del=currentTime-last_check_client;
@@ -361,12 +457,15 @@ public:
         int index = find_group(group);
         std::string res=clients_group[index].get_events_json(hash_worker);
         char* ad=&res[0];
+        ev=clients_group[index].get_events_json(hash_worker);
+        mt.unlock();
         if (index != -1)
-            return clients_group[index].get_events_json(hash_worker);
+            return ev;
         return "{}";
     }
     t_json start_event(std::string group,std::string hash_worker, std::string event_id)
     {
+        mt.lock();
         t_json res;
         res["$status"] = -1;
         int index = find_group(group);
@@ -375,10 +474,12 @@ public:
             int s = clients_group[index].start_event(hash_worker,event_id);
             res["$status"] = s;
         }
+        mt.unlock();
         return res;
     }
      t_json clear_event(std::string group,std::string hash_worker, std::string event_id)
     {
+        mt.lock();
         t_json res;
         res["$status"] = -1;
         int index = find_group(group);
@@ -387,10 +488,12 @@ public:
             int s = clients_group[index].clear_event(hash_worker,event_id);
             res["$status"] = s;
         }
+        mt.unlock();
         return res;
     }
     t_json end_event(std::string group, std::string event_id)
     {
+        mt.lock();
         t_json res;
         res["$status"] = -1;
         int index = find_group(group);
@@ -399,10 +502,12 @@ public:
             int s = clients_group[index].end_event(event_id);
             res["$status"] = s;
         }
+        mt.unlock();
         return res;
     }
     int add_new_event(event ev)
     {
+        mt.lock();
 
         std::string group;
         int size_list = ev.json["meta"]["$list_servers"].size();
@@ -428,6 +533,7 @@ public:
                     int index = find_group(group);
                     if (index == -1)
                     {
+                        mt.unlock();
                         return -1;
                     }
                     respon_id r = clients_group[index].get_respon_id();
@@ -435,14 +541,17 @@ public:
                     ev.json["meta"]["$server_hash"] = server_hash;
                 }
             }
+            mt.unlock();
             return clients_group[index].add_new_event(ev, server_hash);
         }
+        mt.unlock();
         return -1;
     }
 
 private:
     time_t last_check_client;
     std::string server_hash;
+    mutex_n mt;
     std::vector<group_clients> clients_group;
 };
 // class manager_telegram{
